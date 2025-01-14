97037c49731a206c7695628aac00beccc318b329
IvonneMurga <ivonnemurga27@gmail.com>
2025-01-14 02:38:20 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Obtener elementos
+const showGatitoBtn = document.getElementById('showGatitoBtn');
+const gatitoContainer = document.getElementById('gatitoContainer');
+
+// Mostrar el gatito cuando se presiona el botón
+showGatitoBtn.addEventListener('click', () => {
+    gatitoContainer.style.display = 'block'; // Mostrar el gatito
+});
